493f661a3c83a84c618227f49c9a372b2e2e5d15
Jayrees Peterson <contact@ijyrs.com>
2023-04-01 20:03:17 -0400

    Initial Commit v2

---
+++ src/Poopy.ts
@@ -0,0 +1,7 @@
+export class Poopy {
+
+    public suck(num1: number, num2: number): number {
+        return num1 + num2;
+    }
+
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export class SpotifyClient {
+
+    public getName(): string {
+        return "Spotify";
+    }
+
+}
