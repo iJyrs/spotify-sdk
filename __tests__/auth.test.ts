75828321c5357facf5e2681c56a889f5452d369b
Jayrees Peterson <contact@ijyrs.com>
2023-04-04 03:23:51 -0400

    me dumb

--- __tests__/auth.test.ts
+++ __tests__/auth.test.ts
@@ -18,7 +18,7 @@ test("AuthenticationMethod#_options", () => {
 });
 
 test("login", async () => {
-    const uri = new URL("http://localhost:3000/verify#access_token=BQBB_4jgqymAG3U2PVNThiyf9ofOMBGGylOPvQUUSU3jEkVfNPiSoymegcUq0hiNh6CEngyjRoknk_WuEY3vRDAZzh82EVpg9K5UVlPjPx9MQRhQ6r5U04Olm2JIeF-xkeL523pu3RqGcrgGmDPYlE9VxqdzfqSRaBdyFBF0F67s9DWCSb9nUyPfQlvGCa_rGa5F01HpP-L8KprgwCeyyytm&token_type=Bearer&expires_in=3600");
+    const uri = new URL("");
 
     const authMethod = new ImplictGrantAuthMethod(CLIENT_ID, new URL("http://localhost:3000/verify"), {
         scope: [
